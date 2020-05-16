d9217402127a43533ece8cd36e58d60e21479fff
lhc <marco.li@ximalaya.com>
2020-05-16 21:24:52 +0800

    [feat]组件上浮，lib_audio改成ft_audio

--- ft_audio/src/main/java/com/marco/lib_audio/service/AudioServiceImpl.java
+++ ft_audio/src/main/java/com/marco/lib_audio/service/AudioServiceImpl.java
@@ -1,10 +1,15 @@
 package com.marco.lib_audio.service;
 
+import android.app.Activity;
 import android.content.Context;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.marco.lib_audio.app.AudioHelper;
 import com.marco.lib_audio.mediaplayer.core.AudioController;
 import com.marco.lib_base.audio.AudioService;
+import com.marco.lib_model.ft_audio.model.Track;
+
+import java.util.ArrayList;
 
 @Route(path = "/audio/audio_service")
 public class AudioServiceImpl implements AudioService {
@@ -17,6 +22,16 @@ public class AudioServiceImpl implements AudioService {
     @Override
     public void resumeAudio() {
         AudioController.getInstance().resume();
+    }
+
+    @Override
+    public void startMusicService(ArrayList<Track> audios) {
+        AudioHelper.startMusicService(audios);
+    }
+
+    @Override
+    public void addAudio(Activity activity, Track bean) {
+        AudioHelper.addAudio(activity, bean);
     }
 
     @Override
--- lib_base/src/main/java/com/marco/lib_base/audio/AudioService.java
+++ lib_base/src/main/java/com/marco/lib_base/audio/AudioService.java
@@ -1,9 +1,21 @@
 package com.marco.lib_base.audio;
 
+import android.app.Activity;
+
 import com.alibaba.android.arouter.facade.template.IProvider;
+import com.marco.lib_model.ft_audio.model.Track;
+
+import java.util.ArrayList;
 
 public interface AudioService extends IProvider {
+
+    String PATH = "/audio/audio_service";
+
     void pauseAudio();
 
     void resumeAudio();
+
+    void startMusicService(ArrayList<Track> audios);
+
+    void addAudio(Activity activity, Track bean);
 }
