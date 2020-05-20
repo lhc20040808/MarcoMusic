2dffb291d608f8ed36718db7a4045a07f27f9160
lhc <marco.li@ximalaya.com>
2020-05-20 21:21:25 +0800

    [fix]解决打包失败的问题

--- marco_voice/src/main/java/com/marco/voice/application/VoiceApplication.java
+++ marco_voice/src/main/java/com/marco/voice/application/VoiceApplication.java
@@ -1,11 +1,10 @@
 package com.marco.voice.application;
 
-import android.app.Application;
-
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.marco.lib_audio.app.AudioHelper;
+import com.qihoo360.replugin.RePluginApplication;
 
-public class VoiceApplication extends Application {
+public class VoiceApplication extends RePluginApplication {
 
     private static VoiceApplication mApplication = null;
 
